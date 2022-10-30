9476cff0db36c02070b42ae5ae0313db71bf4af0
rhutik03 <91327830+rhutik03@users.noreply.github.com>
2022-10-30 12:03:43 +0530

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,25 @@
+document.write('<header>\
+<nav class="nav">\
+    <a href="index.html" class="logo"><img src="infinite.png" alt="Infinite">SPACE</a>\
+    <div class="hamburger">\
+        <span class="line"></span>\
+        <span class="line"></span>\
+        <span class="line"></span>\
+    </div>\
+\
+    <div class="nav_link hide">\
+        <a href="#" class="planets">Planets</a>\
+        <a href="#">Galaxies</a>\
+        <a href="#">About</a>\
+    </div>\
+\
+    <div class="planetsli hidepl">\
+        <a href="merc.html">MERCURY</a>\
+        <a href="venus.html">VENUS</a>\
+        <a href="earth.html">EARTH</a>\
+        <a href="mars.html">MARS</a>\
+        <a href="#">JUPITER</a>\
+        <a href="#">SATURN</a>\
+        <a href="#">URANUS</a>\
+        <a href="#">NEPTUNE</a>\
+    </div> </nav></header>');
